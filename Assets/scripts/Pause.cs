634a3521d4a3fef5e626bcde9e8e7e1a8e29b5ac
Eliot1111 <barkanov90@bk.ru>
2022-08-27 00:33:14 +0300

    Ui menu

---
+++ Assets/scripts/Pause.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public bool PauseGame;
+    public GameObject pauseGameMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause1();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseGameMenu.SetActive(false);
+        Time.timeScale = 1f;
+        PauseGame = false;
+    }
+
+    public void Pause1()
+    {
+        pauseGameMenu.SetActive(true);
+        Time.timeScale = 0f;
+        PauseGame = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+}
